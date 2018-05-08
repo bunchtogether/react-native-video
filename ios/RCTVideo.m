c6170780f60ddf8c017aaf15ba6f14802811eaac
John Wehr <johnwehr@gmail.com>
2018-05-08 05:39:18 -0400

    Disable automatic wait

--- ios/RCTVideo.m
+++ ios/RCTVideo.m
@@ -96,7 +96,7 @@ static NSString *const timedMetadata = @"timedMetadata";
     playerLayer.rctDelegate = self;
     playerLayer.view.frame = self.bounds;
     playerLayer.player = _player;
-    //playerLayer.player.automaticallyWaitsToMinimizeStalling = NO;
+    playerLayer.player.automaticallyWaitsToMinimizeStalling = NO;
     playerLayer.view.frame = self.bounds;
     return playerLayer;
 }
